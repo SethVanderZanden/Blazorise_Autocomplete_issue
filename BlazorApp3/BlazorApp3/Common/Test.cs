c612e7c33c0940a6df98b2372d5f10196f07c05b
Seth VanderZanden <sethvanderzanden28@gmail.com>
2024-01-24 10:40:24 -0500

    Init

---
+++ BlazorApp3/BlazorApp3/Common/Test.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Transport.Quic;
+
+namespace BlazorApp3.Common;
+
+public class CountryData
+{
+
+    public CountryData()
+    {
+        Name = "";
+        ISO = "";
+    }
+    
+    public CountryData(string n, string iso)
+    {
+        Name = n;
+        ISO = iso;
+    }
+    
+    public string Name { get; set; }
+    public string ISO { get; set; }
+}
+
+public class CountryService()
+{
+    public async Task<List<CountryData>> GetCountryDataAsync()
+    {
+        await Task.Delay(2);
+
+        return new()
+        {
+            new("Canada", "CA"), new("United States", "USA"), new("Mexico", "MX")
+        };
+    }
+}
